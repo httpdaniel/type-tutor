cfbd1fda4b839a4c2c5e9875e6844f425071e8b7
httpdaniel <daniel.097@hotmail.com>
2021-03-02 13:48:28 +0000

    Adding navbar

---
+++ type-tutor-ui/src/components/Navbar.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+function Navbar() {
+  return (
+    <div>
+      <ul>
+        <Link to="/login">
+          <li>Login</li>
+        </Link>
+        <Link to="/register">
+          <li>Register</li>
+        </Link>
+        <Link to="/">
+          <li>Home</li>
+        </Link>
+      </ul>
+    </div>
+  );
+}
+
+export default Navbar;
