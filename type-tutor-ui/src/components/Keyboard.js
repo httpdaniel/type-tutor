dde823bfc593e6356ad349e92b35c6076ef1f063
httpdaniel <daniel.097@hotmail.com>
2021-03-09 16:24:50 +0000

    Updating keyboard

--- type-tutor-ui/src/components/Keyboard.js
+++ type-tutor-ui/src/components/Keyboard.js
@@ -1,20 +1,24 @@
-/* eslint-disable*/
 import React, { useRef, useState } from 'react';
 import Keyboard from 'react-simple-keyboard';
 import 'react-simple-keyboard/build/css/index.css';
+import { Button } from '@material-ui/core';
 import SampleWords from './SampleWords';
-
-import './KeyboardComplete.css';
+import 'font-awesome/css/font-awesome.min.css';
+import '../styles/KeyboardComplete.scss';
+import '../styles/App.scss';
 
 function KeyboardComplete() {
   const [input, setInput] = useState('');
   const [currentWordNumber, setcurrentWordNumber] = useState(0);
 
+  const [focused, setFocused] = useState(false);
+  const [focusPopup, setFocusPopup] = useState(true);
   const keyboard = useRef();
-  const wordList = ['random', 'words', 'tooo'];
-  var correct = true;
-  var word_correct = false;
-  var proceedNext = false;
+  const text = 'seem year keep if also give between those what ask get one or stand now like state or how system and can great late under mean so be find know do person must make number give system get turn lead fact a head call all good tell run want when need each early very may you problem off life eye';
+  const wordList = text.split(' ');
+  const correct = true;
+  const word_correct = false;
+  let proceedNext = false;
 
   const onChangeInput = (event) => {
     const inputTB = event.target.value;
@@ -41,16 +45,16 @@ function KeyboardComplete() {
       }
       console.log(window.correct, currentCorrectArray.length);
       if (
-        currentWord === ' ' &&
-        window.correct === true &&
-        typedLetters.length - 1 === currentCorrectArray.length
+        currentWord === ' '
+        && window.correct === true
+        && typedLetters.length - 1 === currentCorrectArray.length
       ) {
         window.word_correct = true;
         proceedNext = true;
       } else if (
-        currentWord === ' ' &&
-        window.correct === false &&
-        typedLetters.length - 1 === currentCorrectArray.length
+        currentWord === ' '
+        && window.correct === false
+        && typedLetters.length - 1 === currentCorrectArray.length
       ) {
         window.word_correct = false;
         proceedNext = true;
@@ -65,13 +69,28 @@ function KeyboardComplete() {
 
   return (
     <div className="keyboard">
-      <SampleWords
-        currentWord={wordList[currentWordNumber]}
-        wordList={wordList}
-        letterCorrect={window.correct}
-        wordCorrect={window.word_correct}
-      />
-      <input value={input} onChange={onChangeInput} autoFocus />
+      <div className="main__text">
+        {focusPopup ? (
+          <div className="out__of__focus">
+            <Button
+              style={{ backgroundColor: 'transparent' }}
+              onClick={() => { setFocused(true); setFocusPopup(false); }}
+            >
+              <i className="fa fa-mouse-pointer" />
+              Click to activate
+            </Button>
+          </div>
+        ) : (<></>)}
+        <div id="words" className={focused ? 'notBlurred' : 'blurred'}>
+          <SampleWords
+            currentWord={wordList[currentWordNumber]}
+            wordList={wordList}
+            letterCorrect={window.correct}
+            wordCorrect={window.word_correct}
+          />
+        </div>
+      </div>
+      <input value={input} onChange={onChangeInput} />
       <div style={{ pointerEvents: 'none' }}>
         <Keyboard
           keyboardRef={(r) => (keyboard.current = r)}
