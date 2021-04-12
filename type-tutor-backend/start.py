994de9e80df360be6aea91a7814cfd33ba73f33a
joshuacassidy <joshcassidy79@gmail.com>
2021-04-12 22:21:59 +0100

    added cors to flask

--- type-tutor-backend/start.py
+++ type-tutor-backend/start.py
@@ -5,8 +5,11 @@ import mysql.connector
 from mysql.connector import Error
 import jwt
 from datetime import datetime, timedelta
+from flask_cors import CORS
 
 app = Flask(__name__)
+cors = CORS(app, resources={r"/*": {"origins": "*"}})
+
 
 @app.route('/login', methods=['POST'])
 def login():
